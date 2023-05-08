863e529c4545167e1d91508f67bdc836b49c0622
Lumin0l <idelaiglesia.m@gmail.com>
2023-05-08 11:16:16 +0200

    variables 1

---
+++ ex00_elements/elements_00_variables.js
@@ -0,0 +1,20 @@
+/*
+Variables can be defined by the use of the keyword "var"
+*/
+
+var my_name = "My name is Imanol";
+
+console.log(my_name);
+
+/*
+Variables in JS can also be declared "in bulk"
+*/
+
+var first_name, last_name, email;
+first_name = 'Imanol';
+last_name = "de la Iglesia";
+email = "delaiglesia@gmail.com";
+
+console.log(first_name);
+console.log(last_name);
+console.log(email);
