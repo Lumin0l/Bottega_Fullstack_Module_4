614a1f99496bd4328211dde21d72abf1d4f3d425
Lumin0l <idelaiglesia.m@gmail.com>
2023-05-08 11:36:39 +0200

    comments

---
+++ ex01_syntax/syntax_00_comments.js
@@ -0,0 +1,7 @@
+/*
+As shown in the comments in here.
+*/
+
+// Note: comments are useful only in moderation, for really key elements.
+// Comments create more visual noise and are rarely updated so they lead to bugs.
+// To avoid using too many comments and still have readable code, we should be very explicit with the naming of the vars and elements.
