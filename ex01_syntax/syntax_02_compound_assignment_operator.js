8152e094f2e44242f6d528ff25b9c33a282fb8ed
Lumin0l <idelaiglesia.m@gmail.com>
2023-05-08 15:47:48 +0200

    compound assignment operators

---
+++ ex01_syntax/syntax_02_compound_assignment_operator.js
@@ -0,0 +1,21 @@
+/*
+How to assign values to vars and have those assignments perform actions.
+*/
+
+// Example data:
+var name = 'Tiffany';
+var sum = 0;
+var gradeOne = 100;
+var gradeTwo = 80;
+
+// How to add 2 things:
+	// Let's add gradeOne to sum:
+	sum += gradeOne;
+	console.log(sum);
+
+// All operators work like this:
+	// Let's reverse it:
+	sum -= gradeOne;
+	console.log(sum);
+
+// Same with *, /, %...
