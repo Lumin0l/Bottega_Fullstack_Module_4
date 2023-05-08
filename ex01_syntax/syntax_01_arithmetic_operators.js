808fd6654dec89d0eab682fb1d666c33c495c616
Lumin0l <idelaiglesia.m@gmail.com>
2023-05-08 15:42:13 +0200

    arithmetic operators

---
+++ ex01_syntax/syntax_01_arithmetic_operators.js
@@ -0,0 +1,42 @@
+/*
+Let's see the different operators
+*/
+
+// Addition is +
+console.log(2 + 2);
+
+	// We can add it in ()
+	console.log((2 + 2));
+
+// Substraction is -
+console.log(2 - 3);
+
+// Multiplication is *
+console.log(2 * 2);
+
+// Division is /
+console.log(2 / 2);
+
+// Exponent is **
+console.log(2 ** 3);
+
+// Modulus is %
+console.log(5 % 2);
+
+// Incrementer is ++
+var num = 2;
+num++;
+console.log(num);
+
+// Decrementer is --
+num--;
+console.log(num);
+
+// Negative value is - too
+var negative_num = -num;
+console.log(negative_num);
+
+// Make a string into a num with +
+var str_num = '100';
+var converted_num = +str_num;
+console.log(converted_num);
