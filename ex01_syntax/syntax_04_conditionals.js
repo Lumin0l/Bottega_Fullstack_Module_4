4e5e87d2166d7799da24a9bda5bd681b8adab8b4
Lumin0l <idelaiglesia.m@gmail.com>
2023-05-09 14:08:53 +0200

    conditionals

---
+++ ex01_syntax/syntax_04_conditionals.js
@@ -0,0 +1,32 @@
+/*
+How conditionals work in JS
+*/
+
+var age = 12;
+var ageTwo = '12';
+var ageThree = 15;
+
+// Equality ==
+if (age == ageTwo) {
+	console.log("They are equal");
+}
+
+// Strict equality === -> Checks if they are equal not only in value but also in type
+if (age === ageTwo) {
+	console.log("They are equal") // This won't print because one is num and another is str
+}
+
+// Not Equal !=
+if (age != ageThree) {
+	console.log("They are NOT equal");
+}
+
+// Strict not equal !==
+if (age !== ageTwo) {
+	console.log("They are NOT equal: different type");
+}
+
+// Difference < / > / <= / >=
+if (age >= 25){
+	console.log("You can rent a car");
+}
