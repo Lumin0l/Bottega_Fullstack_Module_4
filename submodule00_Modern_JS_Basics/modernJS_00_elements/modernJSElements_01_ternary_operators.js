e4a48c080b79f8ee2dccac1c06ac5aceb7732be5
Imanol de la Iglesia <idelaiglesia.m@gmail.com>
2023-06-05 16:37:04 +0200

    ternary operators in vars

---
+++ submodule00_Modern_JS_Basics/modernJS_00_elements/modernJSElements_01_ternary_operators.js
@@ -0,0 +1,7 @@
+/* You can save ternary operators inside variables */
+// To revise ternaries, they are sentences that stablish an "if else" statement.
+
+const page = "Home";
+console.log(`class=${ page === 'Home' ? 'master-layout' : 'secondary-layout'}`);
+
+// In this case we have: if the page is equal to "Home" print "master-layout", else print "secondary-layout".
