33874650d0b93a815c095d314498b7f99a13c3e3
Imanol de la Iglesia <idelaiglesia.m@gmail.com>
2023-06-05 16:30:10 +0200

    string interpolation with backtick

---
+++ submodule00_Modern_JS_Basics/modernJS_01_actions/modernJS_actions_00_string_interpolation.js
@@ -0,0 +1,11 @@
+/* How to Add and mix strings in modern JS */
+
+// In old JS:
+const lyrics = "never gonna give you up";
+console.log("i'm " + lyrics);
+
+// New method using the "backtick" "`"
+// With the backtick we can use it as a single string:
+console.log(`I'm ${lyrics}`);
+
+// This will outùt the same as before
